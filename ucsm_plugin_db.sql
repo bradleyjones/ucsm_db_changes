be0a77870499d0c2d37fc6e50eec6496d3b0b9e0
Bradley Jones <jones.bradley@me.com>
2016-03-04 22:07:46 +0000

    add auto increment

--- ucsm_plugin_db.sql
+++ ucsm_plugin_db.sql
@@ -1,13 +1,13 @@
 use neutron;
 ALTER TABLE ml2_ucsm_port_profiles ADD (device_id varchar(64) NOT NULL);
 CREATE TABLE IF NOT EXISTS ml2_ucsm_sp_templates
-(vlan_id int(11) NOT NULL PRIMARY KEY,
+(vlan_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 sp_template varchar(64) NOT NULL,
 device_id varchar(64) NOT NULL,
 updated_on_ucs tinyint(1) NOT NULL,
 port_count int(11) NOT NULL);
 CREATE TABLE IF NOT EXISTS ml2_ucsm_vnic_templates
-(vlan_id int(11) NOT NULL PRIMARY KEY,
+(vlan_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 vnic_template varchar(64) NOT NULL,
 device_id varchar(64) NOT NULL,
 physnet varchar(32) NOT NULL,
