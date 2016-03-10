bcfa0d8ea0cbb093ab6c7fafb7a5738db5be2a12
Bradley Jones <jones.bradley@me.com>
2016-03-10 22:04:42 +0000

    Check device_id column does not already exist

--- ucsm_plugin_db.sql
+++ ucsm_plugin_db.sql
@@ -1,11 +1,25 @@
 use neutron;
-ALTER TABLE ml2_ucsm_port_profiles ADD (device_id varchar(64) NOT NULL);
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS add_columns_if_not_exist $$
+CREATE PROCEDURE add_columns_if_not_exist()
+BEGIN
+    IF NOT EXISTS( (SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA='neutron'
+            AND COLUMN_NAME='device_id' AND TABLE_NAME='ml2_ucsm_port_profiles') ) THEN
+        ALTER TABLE ml2_ucsm_port_profiles ADD device_id varchar(64) NOT NULL;
+    END IF;
+END $$
+DELIMITER ;
+
+CALL add_columns_if_not_exist();
+
 CREATE TABLE IF NOT EXISTS ml2_ucsm_sp_templates
 (vlan_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 sp_template varchar(64) NOT NULL,
 device_id varchar(64) NOT NULL,
 updated_on_ucs tinyint(1) NOT NULL,
 port_count int(11) NOT NULL);
+
 CREATE TABLE IF NOT EXISTS ml2_ucsm_vnic_templates
 (vlan_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 vnic_template varchar(64) NOT NULL,
